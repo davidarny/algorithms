88ebdff558896a075d8ba3871641a73db58114f1
David Arutiunian <arutunjan666@mail.com>
2018-12-07 21:58:34 +0300

    Add Visual Studio Code configuration

--- src/aeroflot/main.cpp
+++ src/aeroflot/main.cpp
@@ -24,10 +24,12 @@ int main(int argc, char* argv[])
     bool debug;
     try {
         cxxopts::Options options("Aeroflot", "Aeroflot flights timetable & path search");
-        options.add_options()("search", "Specifies search type (DFS/BFS)",
-            cxxopts::value<std::string>()->default_value("BFS"))("file", "Path to input file",
-            cxxopts::value<std::string>())(
-            "debug", "Specify if want to see all DFS/BFS steps", cxxopts::value<bool>()->default_value("false"));
+        options.add_options()
+            ("search", "Specifies search type (DFS/BFS)", cxxopts::value<std::string>()->default_value("BFS"))
+            ("file", "Path to input file", cxxopts::value<std::string>())
+            ("debug", "Specify if want to see all DFS/BFS steps", cxxopts::value<bool>()->default_value("false"))
+            ("from", "Source of path", cxxopts::value<std::string>()->default_value(std::string()))
+            ("to", "Destination of path", cxxopts::value<std::string>()->default_value(std::string()));
         auto result = options.parse(argc, argv);
         type = result["search"].as<std::string>();
         if (result.count("file") == 0) {
@@ -35,19 +37,26 @@ int main(int argc, char* argv[])
         }
         auto path = result["file"].as<std::string>();
         debug = result["debug"].as<bool>();
+        from = result["from"].as<std::string>();
+        to = result["to"].as<std::string>();
 
         graph.debug(debug);
         graph.parse(path);
         graph.print();
         std::cout << std::endl;
 
-        std::cout << "From: ";
-        std::cin >> from;
-        std::cout << std::endl;
-
-        std::cout << "To: ";
-        std::cin >> to;
-        std::cout << std::endl;
+        if (from.empty() || to.empty()) {
+            if (from.empty()) {
+                std::cout << "From: ";
+                std::cin >> from;
+                std::cout << std::endl;
+            }
+            if (to.empty()) {
+                std::cout << "To: ";
+                std::cin >> to;
+                std::cout << std::endl;
+            }
+        }
 
         std::cout
             << "Going from "
